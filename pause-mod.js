72423b8f2d45b00c7bed4a0d1fdf3570076f0a29
FatCatX <garretsimpson@yahoo.com>
2022-01-22 15:59:14 -0800

    Update pause-mod.js
    Added step one tick.

--- pause-mod.js
+++ pause-mod.js
@@ -2,15 +2,33 @@ const METADATA = {
     website: "https://github.com/garretsimpson/shapez-mods",
     author: "FatcatX",
     name: "Shapez pause mod",
-    version: "0.1",
+    version: "0.2",
     id: "pause-mod",
-    description: "Press Pause to pause / resume the game.",
+    description: "Press Pause to pause / resume the game.  Press Space to step one tick.",
     minimumGameVersion: ">=1.5.0",
 };
 
 const GameHUDExtention = ({ $super, $old }) => ({
     shouldPauseGame() {
         return $old.shouldPauseGame.call(this) || GameHUDExtention.gamePaused;
+    },
+});
+
+const GameCoreExtention = ({ $super, $old }) => ({
+    initializeRoot(...args) {
+        $old.initializeRoot.call(this, ...args);
+        const root = this.root;
+        this.root.gameState.inputReciever.keydown.addToTop(key => {
+            if (key.keyCode === 32 /* Space */) {
+                GameHUDExtention.gamePaused = false;
+                this.root.time.updateRealtimeNow();
+                this.root.time.performTicks(this.root.dynamicTickrate.deltaMs, this.boundInternalTick);
+                root.productionAnalytics.update();
+                root.achievementProxy.update();
+                GameHUDExtention.gamePaused = true;
+                return shapez.STOP_PROPAGATION;
+            }
+        });
     },
 });
 
@@ -31,6 +49,7 @@ class Mod extends shapez.Mod {
             modifiers: {},
             handler: this.togglePause.bind(this),
         });
+        this.modInterface.extendClass(shapez.GameCore, GameCoreExtention);
         this.modInterface.extendClass(shapez.GameHUD, GameHUDExtention);
     }
 }
