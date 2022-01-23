5c8ebee74bdba551f1a522c0a49a00a45ace477b
FatCatX <garretsimpson@yahoo.com>
2022-01-22 19:47:07 -0800

    Update pause-mod.js
    - Version 0.3
    - gamePaused is now a MOD setting
    - Step is now a registered key binding

--- pause-mod.js
+++ pause-mod.js
@@ -2,54 +2,61 @@ const METADATA = {
     website: "https://github.com/garretsimpson/shapez-mods",
     author: "FatcatX",
     name: "Shapez pause mod",
-    version: "0.2",
+    version: "0.3",
     id: "pause-mod",
     description: "Press Pause to pause / resume the game.  Press Space to step one tick.",
     minimumGameVersion: ">=1.5.0",
+    setttings: { gamePaused: false },
 };
 
 const GameHUDExtention = ({ $super, $old }) => ({
     shouldPauseGame() {
-        return $old.shouldPauseGame.call(this) || GameHUDExtention.gamePaused;
+        return $old.shouldPauseGame.call(this) || METADATA.setttings.gamePaused;
     },
 });
 
 const GameCoreExtention = ({ $super, $old }) => ({
     initializeRoot(...args) {
         $old.initializeRoot.call(this, ...args);
+        this.root.keyMapper.getBinding(shapez.KEYMAPPINGS.mods["step"]).add(this.stepTick, this);
+    },
+
+    stepTick() {
         const root = this.root;
-        this.root.gameState.inputReciever.keydown.addToTop(key => {
-            if (key.keyCode === 32 /* Space */) {
-                GameHUDExtention.gamePaused = false;
-                this.root.time.updateRealtimeNow();
-                this.root.time.performTicks(this.root.dynamicTickrate.deltaMs, this.boundInternalTick);
-                root.productionAnalytics.update();
-                root.achievementProxy.update();
-                GameHUDExtention.gamePaused = true;
-                return shapez.STOP_PROPAGATION;
-            }
-        });
+        METADATA.setttings.gamePaused = false;
+        root.time.updateRealtimeNow();
+        root.time.performTicks(this.root.dynamicTickrate.deltaMs, this.boundInternalTick);
+        root.productionAnalytics.update();
+        root.achievementProxy.update();
+        METADATA.setttings.gamePaused = true;
+        return shapez.STOP_PROPAGATION;
     },
 });
 
 class Mod extends shapez.Mod {
     togglePause() {
-        GameHUDExtention.gamePaused = !GameHUDExtention.gamePaused;
-        console.debug("Game paused:", GameHUDExtention.gamePaused);
+        METADATA.setttings.gamePaused = !METADATA.setttings.gamePaused;
     }
 
     init() {
         console.log("##### Init mod:", METADATA.name);
 
-        GameHUDExtention.gamePaused = false;
         this.modInterface.registerIngameKeybinding({
             id: "pause",
             keyCode: 19, // Pause
             translation: "Pause",
             modifiers: {},
-            handler: this.togglePause.bind(this),
+            handler: this.togglePause,
         });
-        this.modInterface.extendClass(shapez.GameCore, GameCoreExtention);
+        this.modInterface.registerIngameKeybinding({
+            id: "step",
+            keyCode: 32, // Space
+            translation: "Step",
+            modifiers: {},
+            repeated: true,
+        });
+
         this.modInterface.extendClass(shapez.GameHUD, GameHUDExtention);
+        this.modInterface.extendClass(shapez.GameCore, GameCoreExtention);
     }
 }
